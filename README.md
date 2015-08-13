0041252658bd6b2d0698268e27a0d88f45980243
Maryna Kolesnik <saga9119@gmail.com>
2015-08-13 18:59:26 +0300

    Add README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # selenium-ruby
-Simple selenium tests with ruby
+Simple selenium tests with ruby.
+Test object: Ghost is an open source publishing platform (https://ghost.org/)
+
+
+Requirements:
+Ruby
+require 'page-object'
+require 'selenium-webdriver'
+require 'rspec'
+
+
+# How to get started:
+meet all requirements
+$ git clone https://github.com/mkpythonanywhereblog/selenium-ruby.git
+$ cd selenium-ruby
+
+run the test with rspec, Chrome browser is started by default:
+$ rspec test_signin_page_rspec.rb
+
+generate test results report in html format:
+$ rspec test_signin_page_rspec.rb --format html --out test_results.html
+
+In case of any load errors please check your gems version and dependencies.
+
---
+++ test_signin_page_rspec.rb
@@ -0,0 +1,63 @@
+require 'rubygems'
+require 'selenium-webdriver'
+require 'test/unit'
+require 'csv'
+require 'rspec'
+require 'rspec/expectations'
+require_relative 'helper.rb'
+require_relative 'signin_page.rb'
+require_relative 'internal_page.rb'
+
+
+describe "SignIn Page Test" do
+
+
+  before(:all) do  
+	    @helper = Helper.new
+	    @wait = @helper.wait
+	    @sp = @helper.signin_page
+	    @ip = @helper.internal_page
+  end  
+  
+
+  before(:each) do
+  		@sp.open
+  	begin 
+  	    @ip.logout
+  	    @sp.open
+  	rescue
+   	end
+  end
+
+
+  after(:all) do  
+    	@helper.quit
+  end
+
+
+  it "Should signin with valid credentials" do      
+		@sp.signin('saga9119@gmail.com', 'Qwer12345')
+		expect(@ip.is_internal_page(@wait)).to be_truthy		
+  end  
+
+
+  it "Should not signin with invalid credentials" do      
+		user_data.each do |user|
+		  	email = user[0], password = user[1]		  
+		  	@sp.signin(email, password)
+			expect(@sp.is_signin_page(@wait)).to be_truthy		  
+  		end  
+  end
+
+
+	def user_data
+	  user_data = CSV.read Dir.pwd + '/user_data.csv'
+	  descriptor = user_data.shift
+	  descriptor = descriptor.map { |key| key.to_sym }
+	  user_data.map { |user| Hash[ descriptor.zip(user) ] }
+	  return user_data
+    end
+
+end  
+
+  
